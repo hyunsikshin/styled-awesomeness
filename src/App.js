77d59456ea61fb245d064de58840cc56a75ade21
hyunsikshin <hyunsik.dev@gmail.com>
2018-07-06 17:21:56 +0900

    Extra Attributes and Mixins

--- src/App.js
+++ src/App.js
@@ -1,63 +1,44 @@
 import React, { Component } from 'react';
-import styled, { injectGlobal, keyframes } from 'styled-components';
+import styled, { injectGlobal, css } from 'styled-components';
 
 injectGlobal`
   body {
     padding:0;
     margin:0;
   }
-`
+`;
+
+const awesomeCard = css`
+  box-shadow: 0 4px 6px rgba(50,50,93,0.11), 0 1px 3px rgba(0,0,0,0.88);
+  background-color : white;
+  border-radius : 10px;
+  padding: 20px;
+`;
+
+const Container  = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Input = styled.input.attrs({
+  required: true
+})`
+  border : none;
+  ${awesomeCard};
+`;
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <Button >Hello</Button>
-        <Button danger rotationtime={2} >Hello</Button>
-        <Anchor href="https://google.com">Go to Google</Anchor>
+        <Input placeholder="hello"/>
       </Container>
     );
   }
 }
 
-const Container  = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-color: #bdc3c7;
-`
 
-const Button = styled.button`
-border-radius: 50px;
-    padding: 5px;
-    min-width: 120px;
-    color: white;
-    font-weight: 600;
-    -webkit-appearance: none;
-    cursor: pointer;
-    &:active,
-    &:focus {
-      outline: none;
-    };
-    background-color: ${props => props.danger ? "#e74c3c" : "#2ecc71"};
-    ${props => {
-      if(props.danger) {
-        return `animation: ${rotation} ${props.rotationtime}s linear infinite`;
-      }
-    }};
-`;
-
-const Anchor = Button.withComponent("a").extend`
-    text-decoration : none;
-`;
-
-const rotation = keyframes`
-    from {
-      transform : rotate(0deg);
-    }
-    to {
-      transform : rotate(360deg);
-    }
-`;
 
 
 export default App;
